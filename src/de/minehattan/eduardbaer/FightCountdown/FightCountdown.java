e752eb8cc657799c5ad1158e286bcecea4944132
EduardBaer <butterfly_barni@web.de>
2011-03-25 18:59:54 +0100

    change /fight to /fight <seconds>; reach v0.2

--- src/de/minehattan/eduardbaer/FightCountdown/FightCountdown.java
+++ src/de/minehattan/eduardbaer/FightCountdown/FightCountdown.java
@@ -18,52 +18,45 @@ public class FightCountdown extends JavaPlugin {
 
 	@Override
 	public void onDisable() {
-        System.out.println("FightCountdown is disabled!");
+		System.out.println("FightCountdown is disabled!");
 	}
 
 	@Override
 	public void onEnable() {
-        PluginDescriptionFile pdfFile = this.getDescription();
-        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+		PluginDescriptionFile pdfFile = this.getDescription();
+		System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
 		
 	}
 	
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
-	    String[] split = args;
-	    String commandName = command.getName().toLowerCase();
-	        if (sender instanceof Player) {
-	            if (commandName.equals("fight")) {
-	                if (split.length == 0) {
-	                    getServer().broadcastMessage(ChatColor.RED + "5...");
-	                    try{
-	                        Thread.sleep(1000);
-	                    } catch(InterruptedException e){}
-	                    getServer().broadcastMessage(ChatColor.RED + "4...");
-	                    try{
-	                        Thread.sleep(1000);
-	                    } catch(InterruptedException e){}
-	                    getServer().broadcastMessage(ChatColor.RED + "3...");
-	                    try{
-	                        Thread.sleep(1000);
-	                    } catch(InterruptedException e){}
-	                    getServer().broadcastMessage(ChatColor.RED + "2...");
-	                    try{
-	                        Thread.sleep(1000);
-	                    } catch(InterruptedException e){}
-	                    getServer().broadcastMessage(ChatColor.RED + "1...");
-	                    try{
-	                        Thread.sleep(1000);
-	                    } catch(InterruptedException e){}
-	                    getServer().broadcastMessage(ChatColor.RED + "Fight!");
+		String[] split = args;
+		String commandName = command.getName().toLowerCase();
+		if (sender instanceof Player) {
+			if (commandName.equals("fight")) {
+				if (split.length == 0) {
+					return false;
+				}
+				else {
+					int count = 0;
+					try{
+					count = Integer.valueOf(split[0]).intValue();
+					} catch(NumberFormatException e) {return false;}
+					for (int i = count; i > 0; i--) {
+						getServer().broadcastMessage(ChatColor.RED + "" + i + "...");
+						try{
+							Thread.sleep(1000);
+						} catch(InterruptedException e){}
+					}
+					getServer().broadcastMessage(ChatColor.RED + "Fight!");
 	                    
-	                }
+				}
 
-	            }
-
-	        }
-	        return true;
-	    }
+			}
+			return true;
+		}
 	
-
+		return false;
+	}
 
 }
+
